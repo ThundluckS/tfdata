f8cfe12f066f56d913d0befa5be0d35dfc77bfc8
ThundluckS <thundluck.s@gmail.com>
2019-09-18 11:20:47 +0700

    edit label map

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -31,8 +31,6 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'totoro':
         return 1
-    else if row_label == 'nekobus':
-        return 2
     else:
         None
 
